5088af0edf06882b803009d72447b9ea0ef8a508
Diether Besa <besadiether031@gmail.com>
2023-10-12 13:15:12 +0800

    Added pages for login and forgot password and setting a routes for each pages.

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
+    return view('current.admin-login');
+});
+
+Route::get('/forgotpassword', function () {
+    return view('current.admin-forgotpassword');
+});
+
+Route::get('/verifycode', function () {
+    return view('current.admin-entersecuritycode');
+});
+
+Route::get('/resetpassword', function () {
+    return view('current.admin-resetpassword');
+});
+
+Route::get('/employeetable', function () {
     return view('current.employee-table');
 });
 
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit05ec9ffc7f67536258d2db2d90891184
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit05ec9ffc7f67536258d2db2d90891184::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire05ec9ffc7f67536258d2db2d90891184($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit05ec9ffc7f67536258d2db2d90891184::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire05ec9ffc7f67536258d2db2d90891184($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }

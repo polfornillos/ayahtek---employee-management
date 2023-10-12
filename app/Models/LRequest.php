bddd8e577442fe11f23533ae63a7907745f09b33
R.A Borja <borja.ronaldandrew@gmail.com>
2023-10-12 19:26:54 +0800

    - Rename model from LeaveRequest to LRequest to match the name of the table. - run composer dump-autoload to make sure the model is still registered in the app

--- app/Models/LRequest.php
+++ app/Models/LRequest.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class LeaveRequest extends Model
+class LRequest extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }

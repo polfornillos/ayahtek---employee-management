52c2cbe35c66cfd2a2e54183d7562662eaa5ac0f
Paul Fornillos <paul.fornillosb@gmail.com>
2023-10-12 10:14:16 +0800

    Created Model, Added Model in Controller

--- app/Http/Controllers/employeeTableController.php
+++ app/Http/Controllers/employeeTableController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use app\Models\AEmployees;
+
 class employeeTableController extends Controller
 {
     public function index() { 
         return view('welcome');
-        
+     
     }
 }

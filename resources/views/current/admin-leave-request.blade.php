c15892806cb32e4df1861e0c657369b95b469490
R.A Borja <borja.ronaldandrew@gmail.com>
2023-10-13 10:05:46 +0800

    added bootstrap and jquery

--- resources/views/current/admin-leave-request.blade.php
+++ resources/views/current/admin-leave-request.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.main-base')
 
 @section('script')
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+
+  <!-- jquery -->
+  <script
+    src="https://code.jquery.com/jquery-3.7.1.js"
+    integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+
 @endsection
 
 @section('content')
@@ -61,4 +73,12 @@
             </div>
         </div> 
     </div>
+@endsection
+
+@section('jquery')
+  <script>
+    $(document).ready(function() {
+      // code goes here ...
+    });
+  </script>
 @endsection

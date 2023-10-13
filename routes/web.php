fd95f7a9fbba0152f8bf25714db5a8d83390f483
R.A Borja <borja.ronaldandrew@gmail.com>
2023-10-13 15:58:39 +0800

    remove } to properly render route

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/employeetable', function () {
 
 // group route that utilizes the same controller class
 Route::controller(LeaveRequestController::class)->group(function () {
-    Route::get('/leave-request}', 'index');
+    Route::get('/leave-request', 'index');
     Route::post('/leave-request/add', 'store');
 });
 

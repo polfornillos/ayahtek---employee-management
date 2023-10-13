3553fd53f750c9fbda3cc92dcc47d1cbd3948bd3
john-dela-cruz <johnbaldozadelacruz@gmail.com>
2023-10-13 15:12:52 +0800

    Revert "Removed sample Creation"
    This reverts commit 4cd14a4db8a390fb9757c100e3472c86d0f30b11.

--- app/Http/Controllers/employeeTableController.php
+++ app/Http/Controllers/employeeTableController.php
@@ -24,4 +24,21 @@ class employeeTableController extends Controller
 
         return view('current.admin-employee-table',compact('employees'));
     }
+    // !Test Creation of Employee Juan Dela Cruz
+    /*public function createEmployee(){
+        $employee = new AEmployees;
+        $employee->name = "Juan Dela Cruz";
+        $employee->birthday = "2000,01,01 ";
+        $employee->gender = "Male";
+        $employee->contact = "+638965654177";
+        $employee->status = "active";
+        $employee->salary = 20000;
+        $employee->sss_number = "74-5635493-8";
+        $employee->philhealth_number = "91-871187398-9";
+        $employee->tin = "417-741-554-663";
+
+        $employee->save();
+
+        return view('current.employee-table');
+    }*/
 }

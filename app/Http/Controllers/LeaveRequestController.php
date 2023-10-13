edc11d123f91ec25e71f9562de584d9e83638192
R.A Borja <borja.ronaldandrew@gmail.com>
2023-10-13 11:13:36 +0800

    create controller function to store leave request in db

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -11,4 +11,17 @@ class LeaveRequestController extends Controller
     public function index() {
       return view('current.admin-leave-request');
     }
+
+    // function to add leave request data in db
+    public function store(Request $request) {
+      $data = [
+        'name' => $request->name,
+        'credits' => $request->credits,
+        'tLeave' => $request->tLeave,
+        'daysLeave' => $request->daysLeave,
+        'sLeave' => $request->sLeave,
+        'eLeave' => $request->eLeave,
+        'Reason' => $request->Reason,
+      ];
+    }
 }

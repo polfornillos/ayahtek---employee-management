55a1ca74972450e0b253dfbd6d83c7032f0ea7d8
Paul Fornillos <paul.fornillosb@gmail.com>
2023-10-13 10:23:53 +0800

    Created route to employee table function

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\employeeTableController;
 
-Route::get('/employeetable', function () {
-    return view('current.employee-table');
-});
-
-
-Route::get('/welcome', [employeeTableController::class, 'index']); 
+Route::get('/employeetable', [employeeTableController::class, 'employeeTable']); 
+Route::get('/', [employeeTableController::class, 'index']); 

a5ef317d88c9c826093130fcefe6f334976b7008
Paul Fornillos <paul.fornillosb@gmail.com>
2023-10-13 10:27:32 +0800

    Merge branch 'leave-request'
    # Conflicts:
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\employeeTableController;
 use App\Http\Controllers\LeaveRequestController;
 
-
-
 Route::get('/', function () {
     return view('current.admin-login');
 });
